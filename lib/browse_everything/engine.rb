92ba8eb7451573d91df1edb1a656dd29cb06bd06
GriffinJ <jrgriffiniii@gmail.com>
2019-02-09 00:40:35 -0500

    Fixing the engine and SCSS imports

--- lib/browse_everything/engine.rb
+++ lib/browse_everything/engine.rb
@@ -4,6 +4,5 @@ module BrowseEverything
   class Engine < ::Rails::Engine
     config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
     config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
-    config.assets.precompile += %w[browse_everything.js browse_everything.css]
   end
 end
