f8187be977d680ef0773beacdeaeddf4fc440d57
GriffinJ <jrgriffiniii@gmail.com>
2019-02-09 01:26:01 -0500

    Restoring the CSS placeholder for precompiled assets in new apps

--- lib/browse_everything/engine.rb
+++ lib/browse_everything/engine.rb
@@ -4,5 +4,6 @@ module BrowseEverything
   class Engine < ::Rails::Engine
     config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
     config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
+    config.assets.precompile += %w[browse_everything.js browse_everything.css]
   end
 end
