d3577449af3321cdc5ed029421a32efd4f9722c2
James R. Griffin III <jrgriffiniii@gmail.com>
2020-11-02 09:54:43 -0500

    More bug fixes

--- lib/browse_everything/engine.rb
+++ lib/browse_everything/engine.rb
@@ -2,8 +2,22 @@
 
 module BrowseEverything
   class Engine < ::Rails::Engine
-    config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
-    config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
-    config.assets.precompile += %w[browse_everything.js browse_everything.css]
+=begin
+rails aborted!
+Don't know how to build task 'webpacker:install' (See the list of available tasks with `rails --tasks`)
+=end
+    if Rails.version < '6.0'
+      config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
+      config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
+      config.assets.precompile += %w[browse_everything.js browse_everything.css]
+    else
+
+      # This is used for webpacker:install
+      initializer "browse_everything.assets.precompile" do |app|
+        app.config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
+        app.config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
+        app.config.assets.precompile += %w[browse_everything.js browse_everything.css]
+      end
+    end
   end
 end
